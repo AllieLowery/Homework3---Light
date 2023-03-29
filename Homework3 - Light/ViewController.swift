6925c065836089f53484f3659a835edca4803101
Allison Lowery <a.lowery@setonhill.edu>
2023-03-28 21:06:23 -0400

    Finished

--- Homework3 - Light/ViewController.swift
+++ Homework3 - Light/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var lightOn = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        UpdateUI()
         // Do any additional setup after loading the view.
     }
 
-
+    func UpdateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+      
+    }
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn.toggle()
+        UpdateUI()
+        
+    }
+    
+    
 }
 
